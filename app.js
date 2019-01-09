da457725d59c19b7aea1d2543c69cacbf7e8d1f1
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-01-08 17:27:10 -0800

    Complete adding 2 nested item components

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 // TODO
 // TODO
-var GroceryList = (props) => (
+// var GroceryList = (props) => (
+//   <div>
+//     <li>{props.item1}</li>
+//     <li>{props.item2}</li>
+//   </div>
+// );
+// // var item1 = 'banana';
+// // var item2 = 'apple';
+// ReactDOM.render(<GroceryList item1 = {'banana'} item2 = {'apple'}/>, document.getElementById("app"));
+
+
+var Cucumber = (props) => (
+  <li>{props.item}</li>
+)
+
+var Kale = (props) => (
+  <li>{props.item}</li>
+)
+
+var GroceryList = () => (
   <div>
-    <li>{props.item1}</li>
-    <li>{props.item2}</li>
+    <Cucumber item = "cucumber"/>
+    <Kale item = "kale"/>
   </div>
 );
-// var item1 = 'banana';
-// var item2 = 'apple';
-ReactDOM.render(<GroceryList item1 = {'banana'} item2 = {'apple'}/>, document.getElementById("app"));
+
+ReactDOM.render(<GroceryList/>, document.getElementById("app"));
