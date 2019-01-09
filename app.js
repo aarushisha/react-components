f012511297da855c35ce3edae7eb7ee2e1e3f3ea
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-01-08 17:12:36 -0800

    Complete creating the grocery list component

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // TODO
+// TODO
+var GroceryList = (props) => (
+  <div>
+    <li>{props.item1}</li>
+    <li>{props.item2}</li>
+  </div>
+);
+// var item1 = 'banana';
+// var item2 = 'apple';
+ReactDOM.render(<GroceryList item1 = {'banana'} item2 = {'apple'}/>, document.getElementById("app"));
